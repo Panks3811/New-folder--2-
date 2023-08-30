934091833db9374cb07eca1728ca765ceb3117f2
Panks3811 <pankajs3811@gmail.com>
2023-08-30 12:12:45 +0530

    Request / Response

--- app.js
+++ app.js
@@ -13,6 +13,37 @@ const server = http.createServer((req, res) => {
         res.write('</html>');
        return  res.end();
     }
+    if (url === '/home')
+    {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>home</title><head>');
+        res.write('<body><h1>Welcome home</h1></body>')
+        res.write('<body><form action="/home" method="POST"><input type="text" name="home"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+       return  res.end();
+    }
+    if (url === '/about')
+    {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>about</title><head>');
+        res.write('<body><h1>Welcome to About Us page</h1></body>')
+        res.write('<body><form action="/about" method="POST"><input type="text" name="about"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+       return  res.end();
+    }
+    if (url === '/node')
+    {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>node</title><head>');
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>')
+        res.write('<body><form action="/node" method="POST"><input type="text" name="node"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+       return  res.end();
+    }
+
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title><head>');
